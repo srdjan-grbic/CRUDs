4516b423454427ea6ebec50ea1b5f4da7a424de7
Srdjan Grbic <srdjan2945@gmail.com>
2024-09-30 16:45:33 +0200

    FE for MERN (MySQL)
    Adding FE for MERN (MySQL) + basic node setup

---
+++ MERN (MySQL)/client/src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+
+        <div className = "crud_buttons_container">
+
+          <button className = 'crud_button create_btn'>Create</button>
+          <button className = 'crud_button read_btn'>Read</button>
+          <button className = 'crud_button update_btn'>Update</button>
+          <button className = 'crud_button delete_btn'>Delete</button>
+
+        </div>
+
+        <div className = 'main_table'>
+
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+          <div className = 'table_row'></div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
